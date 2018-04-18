1d83c62c72dd265535a11cc7d2e2efcad5e98304
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-04-18 20:45:14 +0800

    Add bodyparser and postgres modules

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
 var express     = require("express");
+var bodyParser  = require("body-parser");
 var app         = express();
 
+/*
+ * Body parser setup
+ */
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+/*
+ * Database setup
+ */
+const { Pool, Client } = require("pg");
+const client = new Client({
+    user: "postgres",
+    password: "123",
+    database: "quikdatatest",
+    port: 5432
+});
+client.connect();
+client.query(`
+    CREATE TABLE IF NOT EXISTS dnca (
+        ID serial NOT NULL PRIMARY KEY,
+        info json NOT NULL
+    );
+`, function(err, result) {
+    if (err) {
+        console.log("DB table creation failed: " + err);
+    } else {
+        console.log("Successfully created DNCA table!");
+    }
+});
+
+app.set("dbClient", client);
+
+/*
+ * Handle routes
+ */
 app.get("/dnca", function(req, res, next){
     res.send("Hello");
+});
+
+app.post("/dnca", function(req, res, next) {
+    console.log(req.body);
+    req.app.get("dbClient").query(
+        "INSERT INTO dnca(info) VALUES($1)", [req.body],
+        function(err, result) {
+            if (err) {
+                console.log("DB operation failed: " + err);
+                res.status(400).send(err);
+            } else {
+                console.log("Successfully added DNCA form!");
+                res.status(200).send();
+            }
+        }
+    );
+
 });
 
 app.get("/", function(req, res, next) {
