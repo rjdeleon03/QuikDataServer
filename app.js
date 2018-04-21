16091c83d52d802c31133b95e38b89ab0861af35
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-04-22 07:40:06 +0800

    Implement pdf generation from DNCA form

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express     = require("express");
 var bodyParser  = require("body-parser");
-var path        = require('path');
+var path        = require("path");
+var ejs         = require("ejs");
+var fs         = require("fs");
+var pdf         = require("html-pdf");
 var app         = express();
 
-// view engine setup
+/*
+ * View engine setup
+ */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 /*
  * Body parser setup
@@ -71,7 +77,49 @@ app.get("/dnca/:id", function(req, res, next) {
             } else {
                 console.log("Successfully retrieved DNCA form!");
                 console.log(result.rows);
-                res.status(200).render("dnca", {dncas: result.rows});
+                res.status(200).render("dnca", {
+                    dnca: result.rows[0],
+                    url: req.protocol + '://' + req.get('host')
+                });
+            }
+        }
+    );
+});
+
+// Serve css files
+app.get("/dnca/stylesheets/:css_id", function(req, res, next) {
+    res.writeHead(200, {'Content-type' : 'text/css'});
+    var fileContents = fs.readFileSync('./public/stylesheets/' + req.params.css_id, {encoding: 'utf8'});
+    res.write(fileContents);
+    res.end();
+});
+
+app.get("/dnca/:id/download", function(req, res, next) {
+    req.app.get("dbClient").query(
+        "SELECT * FROM dnca WHERE ID=$1", [req.params.id],
+        function(err, result) {
+            if (err) {
+                console.log("DB operation failed: " + err);
+                res.status(400).send(err);
+            } else {
+                console.log("Successfully retrieved DNCA form!");
+                
+                // Generate pdf file
+                var data = result.rows[0];
+                var compiled = ejs.compile(fs.readFileSync(__dirname + "/views/dnca.ejs", "utf-8"));
+                var html = compiled({
+                    dnca: data,
+                    url: req.protocol + '://' + req.get('host')
+                });
+                
+                // Render pdf
+                res.setHeader('Content-type', 'application/pdf');
+                pdf.create(html).toStream(function(err, stream){
+                    stream.pipe(res);
+                });
+
+                // Redirect to DNCA form page
+                // res.redirect("/dnca/" + req.params.id);
             }
         }
     );
